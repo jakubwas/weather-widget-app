78b83106e52566d1c3bd005b6b1852b7e65b7e53
Jakub Was <jakubwas.jw@gmail.com>
2020-08-07 21:53:53 +0200

    Change bg depending on day time

--- src/app/components/box-left/box-left.component.ts
+++ src/app/components/box-left/box-left.component.ts
@@ -11,16 +11,38 @@ export class BoxLeftComponent implements OnInit {
   today: any;
   weekDay: string;
   date: string;
+  timeOfTheDay: number;
+  morning: boolean;
+  afternoon: boolean;
+  evening: boolean;
 
   constructor() {}
 
   ngOnInit(): void {
     this.today = new Date();
+    this.timeOfTheDay = this.today.getHours();
     this.weekDay = this.today.toLocaleString('default', { weekday: 'long' });
 
     const mm = this.today.toLocaleString('default', { month: 'short' });
     const dd = this.today.toLocaleString('default', { day: 'numeric' });
     const yyyy = this.today.toLocaleString('default', { year: 'numeric' });
     this.date = `${dd} ${mm} ${yyyy}`;
+
+    this.checkDayTime();
+  }
+
+  checkDayTime() {
+    if (this.timeOfTheDay >= 20 || this.timeOfTheDay < 4) {
+      this.afternoon = false;
+      this.evening = true;
+      console.log(this.timeOfTheDay);
+    } else if (this.timeOfTheDay >= 4 && this.timeOfTheDay < 12) {
+      this.evening = false;
+      this.morning = true;
+      console.log(this.timeOfTheDay);
+    } else {
+      this.morning = false;
+      this.afternoon = true;
+    }
   }
 }
